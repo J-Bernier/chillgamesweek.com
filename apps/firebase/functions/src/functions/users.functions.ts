d3a9cc3786f52cac8b5b9590ba9a3f2bc755bc6e
Jérémy Bernier <jeremy.bernier@capgemini.com>
2022-07-25 15:54:13 +0200

    Updated firebase-functions package and some confs

--- apps/firebase/functions/src/functions/users.functions.ts
+++ apps/firebase/functions/src/functions/users.functions.ts
@@ -1,7 +1,7 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { DB_TEAMS_REF, DB_USERS_REF } from "../const/const";
+import {DB_TEAMS_REF, DB_USERS_REF} from "../const/const";
 
 /**
  * Updates the last name of a user on write
@@ -9,11 +9,11 @@ import { DB_TEAMS_REF, DB_USERS_REF } from "../const/const";
 export const formatUserLastName = functions.database
     .ref(`/${DB_USERS_REF}/{username}/lastName`)
     .onUpdate(async (snap) => {
-        let lastName = snap.after.val() as string;
-        if (lastName === null) {
-            return;
-        }
-        return snap.after.ref.set(lastName.toUpperCase());
+      const lastName = snap.after.val() as string;
+      if (lastName === null) {
+        return;
+      }
+      return await snap.after.ref.set(lastName.toUpperCase());
     });
 
 /**
@@ -22,13 +22,12 @@ export const formatUserLastName = functions.database
 export const addTeam = functions.database
     .ref(`/${DB_USERS_REF}/{username}/teams/{teamName}`)
     .onCreate(async (snap, context) => {
-        const username = context.params.username as string;
-        const teamName = context.params.teamName as string;
+      const {username, teamName} = context.params as {[option: string]: string};
 
-        return await admin
-            .database()
-            .ref(`/${DB_TEAMS_REF}/${teamName}/${DB_USERS_REF}/${username}`)
-            .set(true);
+      return await admin
+          .database()
+          .ref(`/${DB_TEAMS_REF}/${teamName}/${DB_USERS_REF}/${username}`)
+          .set(true);
     });
 
 /**
@@ -37,25 +36,24 @@ export const addTeam = functions.database
 export const removeTeam = functions.database
     .ref(`/${DB_USERS_REF}/{username}/teams/{teamName}`)
     .onDelete(async (snap, context) => {
-        const username = context.params.username as string;
-        const teamName = context.params.teamName as string;
+      const {username, teamName} = context.params as {[option: string]: string};
 
-        await admin
+      await admin
+          .database()
+          .ref(`/${DB_TEAMS_REF}/${teamName}/${DB_USERS_REF}/${username}`)
+          .remove();
+
+      // Delete team if empty
+      const teamUsers = await admin
+          .database()
+          .ref(`/${DB_TEAMS_REF}/${teamName}/${DB_USERS_REF}`)
+          .get();
+
+      if (Object.keys(teamUsers).length === 0) {
+        return await admin
             .database()
-            .ref(`/${DB_TEAMS_REF}/${teamName}/${DB_USERS_REF}/${username}`)
+            .ref(`/${DB_TEAMS_REF}/${teamName}`)
             .remove();
-
-        // Delete team if empty
-        const teamUsers = await admin
-            .database()
-            .ref(`/${DB_TEAMS_REF}/${teamName}/${DB_USERS_REF}`)
-            .get();
-
-        if (Object.keys(teamUsers).length === 0) {
-            return await admin
-                .database()
-                .ref(`/${DB_TEAMS_REF}/${teamName}`)
-                .remove();
-        }
+      }
     });
 
